f8bb01c46703c2a84bf065772dc0c448aa22d452
param-code <sparamveer1001@gmail.com>
2024-08-19 12:48:29 +0530

    some changes made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,21 +8,22 @@ function App() {
       const finalData = await response.json();
       console.log(finalData);
       document.getElementById('here').innerText = finalData.data[0].firstname + " Loves You";
-      // await getImage();
+      await getImage();
   }
 
-    // async function getImage(){
-    //     const res = await fetch('');
-    //     const Data = await res.json();
-    //     console.log(Data);
-    //     document.getElementById('ai').innerHTML = <img src={'https://random.imagecdn.app/500/150'}/>;
-    // }
+    async function getImage(){
+        const res = await fetch('');
+        const Data = await res.json();
+        console.log(Data);
+        document.getElementById('ai').src = 'https://random.imagecdn.app/500/150';
+    }
 
   return (
     <>
       <h1>Welcome to LoveByte</h1>
         <button onClick={getLove} className={'bg-blue-700'}>Get Love</button>
         <br/><br/><br/><br/>
+        <img src={'https://random.imagecdn.app/500/150'} id={'ai'} className={'flex'}/>
         <h2 id={'here'} className={'text-6xl text-center text-amber-950'}></h2>
     </>
   )
