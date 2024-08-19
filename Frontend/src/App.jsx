3fd7219f2eb9710004ef5a9801783c2b8dbe47f0
param-code <sparamveer1001@gmail.com>
2024-08-19 13:12:38 +0530

    some changes made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,21 +12,21 @@ function App() {
   }
 
     async function getImage(){
-        const res = await fetch('');
-        const Data = await res.json();
-        console.log(Data);
+        // const res = await fetch('');
+        // const Data = await res.json();
+        // console.log(Data);
         document.getElementById('ai').src = 'https://random.imagecdn.app/500/150';
     }
 
   return (
     <>
-      <h1>Welcome to LoveByte</h1>
-        <button onClick={getLove} className={'bg-blue-700'}>Get Love</button>
+      <h1 className={'animate-ping py-10 text-3xl bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-xl'}>Welcome to LoveByte</h1>
+        <button onClick={getLove} className={'bg-blue-700 rounded-xl text-3xl px-3 my-5 animate-pulse'}>Get Love</button>
         <br/><br/><br/><br/>
-        <img src={'https://random.imagecdn.app/500/150'} id={'ai'} className={'flex'}/>
-        <h2 id={'here'} className={'text-6xl text-center text-amber-950'}></h2>
+        <img src={'https://random.imagecdn.app/500/150'} id={'ai'} className={'my-3 mx-5'}/>
+        <h2 id={'here'} className={'text-6xl text-center text-amber-950 font-bold py-12 border-2 animate-bounce rounded-xl bg-gradient-to-r from-purple-500 to-pink-500'}>Click on Get Love to Get Love</h2>
     </>
   )
 }
 
-export default App
+export default App;
