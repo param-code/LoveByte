68c31b87a6197b4940c407cb4ac0f9b9c9ee9ba8
param-code <sparamveer1001@gmail.com>
2024-08-19 12:36:07 +0530

    some changes made

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  async function getLove(){
+      const response = await fetch('https://fakerapi.it/api/v1/persons?_gender=female');
+      const finalData = await response.json();
+      console.log(finalData);
+      document.getElementById('here').innerText = finalData.data[0].firstname + " Loves You";
+      // await getImage();
+  }
+
+    // async function getImage(){
+    //     const res = await fetch('');
+    //     const Data = await res.json();
+    //     console.log(Data);
+    //     document.getElementById('ai').innerHTML = <img src={'https://random.imagecdn.app/500/150'}/>;
+    // }
 
   return (
     <>
-      Welcome to LoveByte
+      <h1>Welcome to LoveByte</h1>
+        <button onClick={getLove} className={'bg-blue-700'}>Get Love</button>
+        <br/><br/><br/><br/>
+        <h2 id={'here'} className={'text-6xl text-center text-amber-950'}></h2>
     </>
   )
 }
